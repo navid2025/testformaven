e5a960d5c4ee86f0840138cfd9b6228ecfdfe258
n.zare <n.zare@behsacorp.com>
2023-04-21 14:24:22 +0330

    add distribution manager for release artiface

---
+++ testing/src/main/java/com/navid/importTesting/TestForMavenImporting.java
@@ -0,0 +1,12 @@
+package com.navid.importTesting;
+
+/**
+ * @author n.zare 4/21/2023 $
+ */
+
+public class TestForMavenImporting {
+
+    public void printImporting(){
+        System.out.println("it's testing for importing the librarry");
+    }
+}
